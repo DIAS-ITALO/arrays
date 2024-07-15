5fe01c559f6dde545d64e3cf149d119677ad708f
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-07-15 11:00:59 -0300

    arrays basic

---
+++ arrays_basic.js
@@ -0,0 +1,20 @@
+// Arrays são dados por referência
+
+/* const nomes = ['Eduardo', 'Maria', 'Joana'];
+nomes.shift()
+nomes.pop()
+nomes.unshift('Breno');
+nomes.push('Ítalo');
+nomes.push('Ana');
+
+const novo = nomes.slice(1, -1);
+console.log(nomes);
+console.log(novo); */
+
+/* const nome = 'Luiz Otavio Miranda';
+const nomes = nome.split(' '); */
+
+const nomes = ['Luiz', 'Otávio', 'Miranda'];
+const nome = nomes.join(' ');
+
+console.log(nome);
